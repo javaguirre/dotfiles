2181507ce6b2f0d6170918b9d24d786da2dc38bd
Javier Aguirre <contacto@javaguirre.net>
2014-08-27 15:14:20 +0200

    Symfony dump snippet, hotkeys for helm-buffers

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,6 +6,7 @@
 (helm-mode 1)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 
 ;; Yasnippet
 (require 'yasnippet)

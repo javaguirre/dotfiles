70e1f9c985050baaf4313dff3026076662839a33
Javier Aguirre <contacto@javaguirre.net>
2014-10-26 18:28:39 +0100

    Changing fish theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,12 +2,13 @@
 set fish_path $HOME/.oh-my-fish
 
 # Theme
-set fish_theme bobthefish
+# set fish_theme bobthefish
+set fish_theme clearance
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins git
+set fish_plugins git autojump archlinux extract tmux vi-mode
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
